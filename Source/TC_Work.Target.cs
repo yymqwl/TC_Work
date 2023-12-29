9150e4e99e49e6f4980289b02e55dc581434df03
yymqwl <521008318@qq.com>
2023-12-29 11:07:34 +0800

    测试下

---
+++ Source/TC_Work.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TC_WorkTarget : TargetRules
+{
+	public TC_WorkTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.Add("TC_Work");
+	}
+}
---
+++ Source/TC_Work/Private/MyGameInstance.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+
+
+void UMyGameInstance::LoadCustomConfig()
+{
+	/*
+	*/
+	
+}
+
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+	LoadCustomConfig();
+}
