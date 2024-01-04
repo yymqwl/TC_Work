cdf8e21fd0be2e3612fb7cc633136f1b837bcaad
yymqwl <521008318@qq.com>
2024-01-04 18:03:01 +0800

    xx

--- Source/TC_Work/Private/Async/TestAsyncActor.cpp
+++ Source/TC_Work/Private/Async/TestAsyncActor.cpp
@@ -10,6 +10,7 @@ ATestAsyncActor::ATestAsyncActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 	
 	/*
 	if (InputComponent == nullptr)
